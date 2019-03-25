009a0021350ff50caf66e067c75b1810776bdfb1
happy-yuxuan <victor.wuy@icloud.com>
2019-03-25 14:38:27 +0800

    实现按路程的Dijkstra算法

--- CodeCraft-2019.cpp
+++ CodeCraft-2019.cpp
@@ -1,4 +1,9 @@
 #include "iostream"
+#include "SparseGraph.h"
+#include "ReadGraph.h"
+#include "Dijkstra.h"
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
@@ -20,8 +25,28 @@ int main(int argc, char *argv[])
 	std::cout << "answerPath is " << answerPath << std::endl;
 	
 	// TODO:read input filebuf
-	// TODO:process
-	// TODO:write output file
+	int V = 36;
+    SparseGraph<int> g = SparseGraph<int>(V, true);
+    ReadGraph<SparseGraph<int>, int> readGraph(g, carPath, crossPath, roadPath);
+    g.show();
+
+    // TODO:process
+
+    cout<<"Test Dijkstra:"<<endl<<endl;
+    Dijkstra<SparseGraph<int>, int> dij(g,1);
+    for( int i = 1 ; i <= V ; i ++ ){
+        if(dij.hasPathTo(i)){
+            cout<<"Shortest Path to "<<i<<" : "<<dij.shortestPathTo(i)<<endl;
+            dij.showPath(i);
+        }
+        else
+            cout<<"No Path to "<<i<<endl;
+
+        cout<<"----------"<<endl;
+    }
+
+
+    // TODO:write output file
 	
 	return 0;
 }
