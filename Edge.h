c27c054a653dd4f516ca4feac73c0f6346c6cf3a
happy-yuxuan <victor.wuy@icloud.com>
2019-03-25 14:14:44 +0800

    添加 Dijkstra.h && Edge.h && IndexMinHeap.h && ReadGraph.h && SparseGraph.h

---
+++ Edge.h
@@ -0,0 +1,69 @@
+//
+// Created by Victor Wu on 2019-03-25.
+//
+
+#ifndef CODECRAFT_2019_EDGE_H
+#define CODECRAFT_2019_EDGE_H
+
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+// 边
+template<typename Weight>
+class Edge{
+private:
+    int a,b;    // 边的两个端点
+    Weight weight;  // 边的权值
+
+public:
+    // 构造函数
+    Edge(int a, int b, Weight weight){
+        this->a = a;
+        this->b = b;
+        this->weight = weight;
+    }
+    // 空的构造函数, 所有的成员变量都取默认值
+    Edge(){}
+
+    ~Edge(){}
+
+    // 返回第一个顶点
+    int v(){ return a;}
+    // 返回第二个顶点
+    int w(){ return b;}
+    // 返回权值
+    Weight wt(){ return weight;}
+
+    // 给定一个顶点, 返回另一个顶点
+    int other(int x){
+        assert( x == a || x == b );
+        return x == a ? b : a;
+    }
+
+    // 输出边的信息
+    friend ostream& operator<<(ostream &os, const Edge &e){
+        os<<e.a<<"-"<<e.b<<": "<<e.weight;
+        return os;
+    }
+
+    // 边的大小比较, 是对边的权值的大小比较
+    bool operator<(Edge<Weight>& e){
+        return weight < e.wt();
+    }
+    bool operator<=(Edge<Weight>& e){
+        return weight <= e.wt();
+    }
+    bool operator>(Edge<Weight>& e){
+        return weight > e.wt();
+    }
+    bool operator>=(Edge<Weight>& e){
+        return weight >= e.wt();
+    }
+    bool operator==(Edge<Weight>& e){
+        return weight == e.wt();
+    }
+};
+
+#endif //CODECRAFT_2019_EDGE_H
---
+++ ReadGraph.h
@@ -0,0 +1,46 @@
+//
+// Created by Victor Wu on 2019-03-25.
+//
+
+#ifndef CODECRAFT_2019_READGRAPH_H
+#define CODECRAFT_2019_READGRAPH_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cassert>
+
+using namespace std;
+
+template <typename Graph, typename Weight>
+class ReadGraph {
+public:
+    // 从文件filename中读取图的信息, 存储进图graph中
+    ReadGraph(Graph &graph, const string &carPath, const string &crossPath, const string &roadPath){
+
+        FILE *fp;
+        fp = fopen(roadPath.c_str(), "r");
+        int id, length, speed, channel, from, to, isDuplex;
+        char str1[100];
+        fscanf(fp, "%s\n", str1);
+        cout << str1 << endl;
+        while ( fscanf(fp, "(%d, %d, %d, %d, %d, %d, %d)\n",
+                       &id, &length, &speed, &channel, &from, &to, &isDuplex) != EOF) {
+            printf("%d %d %d %d %d %d %d\n", id, length, speed, channel, from, to, isDuplex);
+            int a = from;
+            int b = to;
+            Weight wt = length;
+
+            assert( a > 0 && a <= graph.V() );
+            assert( b > 0 && b <= graph.V() );
+
+            graph.addEdge( a, b, wt);
+        }
+
+        cout << "------------ read end ------------" << endl;
+
+    }
+};
+
+#endif //CODECRAFT_2019_READGRAPH_H
